0f2fba39327609a08932ddb030fb329d7f715d52
Vlad Drozdov <vyudrozdov@edu.hse.ru>
2019-03-20 20:34:58 +0300

    Item_comm

--- app/src/main/java/com/lydone/bmstuandroid/data/Item.java
+++ app/src/main/java/com/lydone/bmstuandroid/data/Item.java
@@ -1,5 +1,20 @@
 package com.lydone.bmstuandroid.data;
 
 public class Item {
-    //TODO
+
+    string name;
+    int price;
+    int weight;
+
+    public Item(string nam, int price, int gramm){
+        name=nam;
+        this.price=price;
+        weight=gramm;
+    }
+
+    @Override
+    public string ToString(){
+        return "Наименование: " + name + " Цена: " + price + " вес: " + weight + "гр."
+    }
+
 }
