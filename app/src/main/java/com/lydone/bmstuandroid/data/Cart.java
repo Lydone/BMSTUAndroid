52f5366768519a038aa82536c14458cf0da231e4
Valerii Belaventsev <umnik20-05@mail.ru>
2019-03-20 20:09:03 +0300

    Initial commit Add empty Cart and Item classes

---
+++ app/src/main/java/com/lydone/bmstuandroid/data/Cart.java
@@ -0,0 +1,6 @@
+package com.lydone.bmstuandroid.data;
+
+public class Cart {
+    // TODO
+    // for storing current buyer's choice
+}
---
+++ app/src/main/java/com/lydone/bmstuandroid/data/Item.java
@@ -0,0 +1,5 @@
+package com.lydone.bmstuandroid.data;
+
+public class Item {
+    //TODO
+}
